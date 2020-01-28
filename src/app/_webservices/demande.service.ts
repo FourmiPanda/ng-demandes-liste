80541c4cc9954b973b561f989e523bcf1ad5c8e8
Hoan My TRAN <hoanmytran.hmt@gmail.com>
2020-01-28 15:37:51 +0100

    demande updated

--- src/app/_webservices/demande.service.ts
+++ src/app/_webservices/demande.service.ts
@@ -1,20 +1,40 @@
-import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { environment } from "../../environments/environment";
+import { Observable } from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class DemandeService {
-
-  constructor(private http: HttpClient) {
-  }
-
+  constructor(private http: HttpClient) {}
 
   /**
    * Retourne les demandes
    */
-  getDemandes(criteria, page) {
-
+  getDemandes(criteria, page): Observable<Demande[]> {
+    return this.http.get<Demande[]>(environment.api.url);
   }
+}
 
+export class Demande {
+  id: string;
+  texte: string;
+  date: string;
+  payload: {
+    adresse: {
+      numVoie: string;
+      nomVoie: string;
+      cp: string;
+      ville: string;
+      pays: string;
+    };
+  };
+  demandeur: {
+    email: string;
+  };
+  type: {
+    href: string;
+    title: string;
+  };
 }
