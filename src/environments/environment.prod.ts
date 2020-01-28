adbee102555de40baaa7c89c6fe8a2b070b3ef4b
Ismail El Khantache <ismail.elkhantache@amplexor.com>
2020-01-28 17:18:15 +0100

    Update api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  api: {
+    back: {
+      url: 'http://imta.49b88b1b31b54bc0a3b6.westeurope.aksapp.io/demandes-backoffice/api'
+    },
+    settings: {
+      url: 'http://imta.49b88b1b31b54bc0a3b6.westeurope.aksapp.io/parametrage/api'
+    }
+  }
 };
