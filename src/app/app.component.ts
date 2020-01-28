9939d3de98edc7edbf6a8b2a25e713ea7de0d657
Ismail El Khantache <ismail.elkhantache@amplexor.com>
2020-01-28 16:05:05 +0100

    Add translation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'ng-demandes-liste';
 
-  constructor(){
+  constructor(private translate: TranslateService) {
+    translate.setDefaultLang('en');
   }
 }
