dc93702c12f8369bc5b777769717bd9d38717cbf
Ismail El Khantache <ismail.elkhantache@amplexor.com>
2020-01-28 14:53:17 +0100

    Add getDemandes function

--- src/app/_webservices/demande.service.ts
+++ src/app/_webservices/demande.service.ts
@@ -1,24 +1,20 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DemandeService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+  }
 
 
   /**
    * Retourne les demandes
    */
-  getDemandes(criteria, page): Observable<any> {
-    //let url = environment.api.camka.url + '/contact';
-    //if (criteria.keyword) {
-    //  url += '?keyword=' + criteria.keyword;
-    //}
-    //return this.http.get<Results>(url, {withCredentials: true}).map((res) => res);
+  getDemandes(criteria, page) {
+
   }
 
 }
